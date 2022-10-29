f138e0291e8ab9439ed765e77b83aa32e7d84c7b
raphaelnnadi <raphaelnnadi1994@gmail.com>
2022-10-29 11:05:52 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '10aaff73-82ae-4166-a98b-b31b2b4034fc';
+export const MODAL_CLOSE_SEC = 2.5;
